f4326d0485a48c32700d5adacf485f969c1052d1
Sunil Gurung <sunil@prezzee.com.au>
2023-03-25 18:39:08 +0800

    Setup project

---
+++ OnboardingView/OnboardingViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingViewApp.swift
+//  OnboardingView
+//
+//  Created by Sunil Gurung on 25/3/2023.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
